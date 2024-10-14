4e07390a72119f3bdc9e4cab3edf39025105999f
mika <mika@mikanoiMac.local>
2024-10-14 12:34:10 +0900

    コミット

--- src/questions/Question_01.js
+++ src/questions/Question_01.js
@@ -1,56 +1,30 @@
 import React from 'react'
 import ExpectedImg from '../resources/img/Expected_Question_01.png'
-import ExpectedImg2 from '../resources/img/image_illust.png'
+//ここに画像（イラスト）インポート指定するよ
 
-
-
-/**
- * 【問１】React要素の作成
- *
- * 定数 message, date, id の内容をもとにReact要素を作成し、画面に描画してください
- * なお、以下の条件を満たすように要素を作成してください
- *
- * 1. コメントの指示に従い、定数の内容を修正すること
- * 2. import されている React モジュールを利用し、Reactの要素を生成すること
- * 3. message の内容は、 h1 タグで表示すること
- * 4. date の内容は、 div タグで表示すること
- * 5. id の内容は、 p タグで表示すること
- *
- * 【ヒント】
- * React 要素の生成は React.createElement でできる
- * JS の変数や定数を描画するときは {} で囲う必要がある
- * JSX 記法で直接記載してもOKとする
- */
 
 export default function Question_01() {
 
   // 定数群
-  const message = "Hello World!";   // 変更しない
-  const date = "2024/10/06";    // 本日の日付 (YYYY/MM/DD)
-  const id = "001";
-  const sum = 2 * 5; 
-  const backgroundColor = "#eea3ac";
-  const fontSize = "52px";
-  const color = "#f6ff00";  // 背景色を設定       // 出席番号
-  const styles = {
-    backgroundColor: "#f0f0f0",  // 背景色
-    fontSize: "24px",            // フォントサイズ
-    color: "#333",               // テキストカラー
-    padding: "20px",             // パディング
-    borderRadius: "10px"         // ボーダーの丸み
-  };
+  const message = "";   // Hello World!と表示できるようにしましょう
+  const date = "";    // 本日の日付 (YYYY/MM/DD)
+  const id = ""; // 名前を入力
+ //const Ageを追加して年齢を表示させてみましょう
+
+ //const locationを追加して現在地を表示させてみましょう
+
+  //const sumを追加して数式を入れてみましょう
+
+   //constを追加して画像を表示させてみましょう
+
+  //constを追加して今の時間を表示させてみましょう
+
 
   return (
     <div>
-      <h1 style={{ backgroundColor: backgroundColor,fontSize,color }}>【問１】React要素の作成</h1>
+      <h1>【問１】React要素の作成JSXで書きますよ！</h1>
       <div className='question-content'>
       {/* ↓↓↓ 描画する内容を書く ↓↓↓ */}
-      <h1 style={styles}>{message}</h1>
-      <div>{date}</div>
-      <p>{id}</p>
-      <p>{sum}</p>
-      <img src={ExpectedImg2} />
-
 
       {/* ↑↑↑ 描画する内容を書く ↑↑↑ */}
       </div>
@@ -59,3 +33,4 @@ export default function Question_01() {
     </div>
   );
 }
+
