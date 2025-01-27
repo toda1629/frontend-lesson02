d3514fd58528d3951af9eef68c327bc1ac35a10c
toda1629 <72779673+toda1629@users.noreply.github.com>
2025-01-28 06:18:08 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "./resources/css/styles.css";
-
 import Header from "./common/Header";
 import Readme from "./common/Readme";
-
 import Q1 from "./questions/Question_01";
 import Q2 from "./questions/Question_02";
 import Q3 from "./questions/Question_03";
 import Q4 from "./questions/Question_04";
-import Q5 from "./questions/Question_05";
+// import Q5 from "./questions/Question_05";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -24,7 +22,7 @@ root.render(
           <Route path="/q2" element={<Q2 />} />
           <Route path="/q3" element={<Q3 />} />
           <Route path="/q4" element={<Q4 />} />
-          <Route path="/q5" element={<Q5 />} />
+          
           <Route path="*" element={<h1>Not Found Page</h1>} />
         </Routes>
       </section>
